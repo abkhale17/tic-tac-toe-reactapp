6652673b10c382588f077fad1a1570c71e0788a6
abkhale17 <abkhale.17@gmail.com>
2020-01-18 13:35:52 +0530

    Updates

---
+++ precache-manifest.80fad84ce0d116726afb3edfdba3552e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d5b9b12f9f3974f8e8271a0cfccaa2",
+    "url": "/tic-tac-toe-reactapp/index.html"
+  },
+  {
+    "revision": "55d73266259d27cec6a2",
+    "url": "/tic-tac-toe-reactapp/static/css/main.c7ade237.chunk.css"
+  },
+  {
+    "revision": "f67ada3a7ef63f184bb5",
+    "url": "/tic-tac-toe-reactapp/static/js/2.757cf861.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe-reactapp/static/js/2.757cf861.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55d73266259d27cec6a2",
+    "url": "/tic-tac-toe-reactapp/static/js/main.20a8c0f4.chunk.js"
+  },
+  {
+    "revision": "adb5becaba063140ccf7",
+    "url": "/tic-tac-toe-reactapp/static/js/runtime-main.a8c17d22.js"
+  }
+]);
